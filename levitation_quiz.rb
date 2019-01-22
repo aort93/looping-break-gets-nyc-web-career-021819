94d14bb81e4877ec8fec3450fbfb0b866cda3f7b
Alonso Ortega <alonsojortega@gmail.com>
2019-01-22 21:26:19 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,10 +1,11 @@
 
 def levitation_quiz
 	#your code here
-  until phrase == "Wingardium Leviosa"
-  puts "What is the spell that enacts levitation?"
-  phrase = gets.chomp
+  loop do
+    puts "What is the spell that enacts levitation?"
+    answer = gets.chomp;
+    break if answer === 'Wingardium Leviosa'
   end
 
-  puts "You passed the quiz"
+  puts 'You passed the quiz!'
 end
